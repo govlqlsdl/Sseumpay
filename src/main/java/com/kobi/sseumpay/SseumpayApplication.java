46969beca2e4d3ae7add117a0e21c06c44f9f6ac
LAPTOP-DHOJEMI6\석호빈 <govlqlsdl@naver.com>
2025-04-05 23:49:29 +0900

    [석호빈] - Sseumpay 프로젝트 추가

---
+++ src/main/java/com/kobi/sseumpay/SseumpayApplication.java
@@ -0,0 +1,13 @@
+package com.kobi.sseumpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseumpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseumpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobi/sseumpay/SseumpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobi.sseumpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseumpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
